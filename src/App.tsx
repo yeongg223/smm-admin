3e64952247596251a6f9b5088cd79cc3f4a1136d
yeongg223 <yeongg223@gmail.com>
2026-03-23 21:52:50 +0900

    feat(민교): SMM 패널 어드민

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import { Button } from "./components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./components/ui/card";
 import { Input } from "./components/ui/input";
 
 function App() {
   return (
-    <div>
-      <Button>Button</Button>
-      <Input />
-    </div>
+    <Card className="w-[340px]">
+      <CardHeader>
+        <CardTitle>LOGIN</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Input placeholder="ID" />
+        <Input placeholder="PASSWORD" />
+      </CardContent>
+      <CardFooter>
+        <Button>LOGIN</Button>
+      </CardFooter>
+    </Card>
   );
 }
 
